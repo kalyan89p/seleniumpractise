6bb3340d26bfd2bdf5ca9962e1f53cdadeb2cb28
Kalyan P <Kalyan P@Kalyanz>
2022-03-18 00:16:52 +0530

    test

---
+++ src/seleniumpracticing/JavaProgs.java
@@ -0,0 +1,12 @@
+package seleniumpracticing;
+
+public class JavaProgs {
+
+	public JavaProgs() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String args[]) {
+		System.setProperty(null, null);
+	}
+
+}
