0f5ded5636b4638c9cfe0618e8792e11a877bf95
Kalyan P <Kalyan P@Kalyanz>
2022-03-19 07:56:14 +0530

    login to box.com

--- src/seleniumpracticing/JavaProgs.java
+++ src/seleniumpracticing/JavaProgs.java
@@ -12,16 +12,13 @@ public class JavaProgs {
 		System.setProperty("webdriver.chrome.driver","c:\\new folder\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		driver.get("https://www.box.com/home");
+		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
 		driver.findElement(By.xpath("//input[@id='login-email']")).sendKeys("kalyan89p@gmail.com");
 		driver.findElement(By.xpath("//button[@id='login-submit']")).click();
 		driver.findElement(By.xpath("//input[@id='password-login']")).sendKeys("Admin@123");
 		driver.findElement(By.xpath("//button[@id='login-submit-password']")).click();
 		driver.findElement(By.xpath("//div[@class='ProfileButton-avatar']/div/span/span"));
-		
-		
-	
-		
 		
 	}
 
