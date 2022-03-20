864a669dac56c7d39d1af4ab63ba75be9a95c631
Kalyan P <Kalyan P@Kalyanz>
2022-03-20 08:02:05 +0530

    page object model

--- src/seleniumpracticing/JavaProgs.java
+++ src/seleniumpracticing/JavaProgs.java
@@ -1,25 +1,30 @@
 package seleniumpracticing;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.http.util.Asserts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class JavaProgs {
+public class JavaProgs extends webElements {
 	static WebDriver driver;
-
-	public static void main(String args[]) {
+	static WebDriverWait wait;
+	public static void main(String args[]) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","c:\\new folder\\chromedriver.exe");
 		 driver = new ChromeDriver();
+		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 wait = new WebDriverWait(driver, 10);
 		driver.get("https://www.box.com/home");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
-		driver.findElement(By.xpath("//input[@id='login-email']")).sendKeys("kalyan89p@gmail.com");
-		driver.findElement(By.xpath("//button[@id='login-submit']")).click();
-		driver.findElement(By.xpath("//input[@id='password-login']")).sendKeys("Admin@123");
-		driver.findElement(By.xpath("//button[@id='login-submit-password']")).click();
-		driver.findElement(By.xpath("//div[@class='ProfileButton-avatar']/div/span/span"));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LoginButtonHomePage)));
+		driver.findElement(By.xpath(LoginButtonHomePage)).click();
+		driver.findElement(By.xpath(EmailID)).sendKeys("kalyan89p@gmail.com");
+		driver.findElement(By.xpath(LoginButton)).click();
+		driver.findElement(By.xpath(Password)).sendKeys("Admin@123");
+		driver.findElement(By.xpath(Submit)).click();
+		driver.findElement(By.xpath(ProfileIcon));
+	
 		
 	}
-
 }
---
+++ src/seleniumpracticing/webElements.java
@@ -0,0 +1,13 @@
+package seleniumpracticing;
+
+public class webElements {
+	
+	static String LoginButtonHomePage = "//a[contains(text(),'Log in')]";
+	static String EmailID = "//input[@id='login-email']";
+	static String LoginButton = "//button[@id='login-submit']";
+	static String Password = "//input[@id='password-login']";
+	static String Submit = "//button[@id='login-submit-password']";
+	static String ProfileIcon = "//div[@class='ProfileButton-avatar']/div/span/span";
+	
+
+}
