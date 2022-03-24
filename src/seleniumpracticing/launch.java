49b0db0c10163c66c854f3648169bd7c9cb7736d
Kalyan P <Kalyan P@Kalyanz>
2022-03-24 08:15:45 +0530

    implemented test ng framework

--- src/seleniumpracticing/launch.java
+++ src/seleniumpracticing/launch.java
@@ -1,65 +1,56 @@
 package seleniumpracticing;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class JavaProgs{
+public class launch{
 	static WebDriver driver;
 	static webElements we;
 	static pageActions pa;
 	static WebDriverWait wait;
+	static FileInputStream fis;
+	static Properties prop;
+	static String emailid;
+	static String password;
+	
 	
 	@Test
-	void launchBrowser()
+	void launchBrowser() throws IOException
 	{
-		
-	}
-	@Test
-	void login(){
-		
-		
-		
-	}
-	
-	public static void main(String args[]) throws InterruptedException, IOException {
-		System.setProperty("webdriver.chrome.driver","c:\\new folder\\chromedriver.exe");
+		 System.setProperty("webdriver.chrome.driver","c:\\new folder\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		 wait = new WebDriverWait(driver, 10);
-		 FileInputStream fis= new FileInputStream("C:\\Users\\patlo\\eclipse-workspace\\seleniumpracticing\\src\\seleniumpracticing\\testdata.properties");
-		 Properties prop = new Properties();
-		 prop.load(fis);
-		 
-		 
-		 String emailid = prop.getProperty("emailid");
-		 String password = prop.getProperty("password");
-		 
-		 
+		fis= new FileInputStream("C:\\Users\\patlo\\eclipse-workspace\\seleniumpracticing\\src\\seleniumpracticing\\testdata.properties");
+		prop = new Properties();
+		prop.load(fis);
+		emailid = prop.getProperty("emailid");
+		password = prop.getProperty("password");
 		 we = new webElements();
 		 pa = new pageActions(driver, wait);
 		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		 
-		driver.get("https://www.box.com/home");
-		driver.manage().window().maximize();
-		
+		 driver.get("https://www.box.com/home");
+		 driver.manage().window().maximize();
+	}
+	
+	@Test( dependsOnMethods= {"launchBrowser"}, enabled=true)
+	//@Parameters({"email","password"})
+	//@DataProvider
+	void afirsttestcase(){
 		pa.wait(we.loginbuttonHomePage);
 		pa.click(we.loginbuttonHomePage);
 		pa.sendkeys(we.emialid,emailid);
 		pa.click(we.loginbutton);
 		pa.sendkeys(we.password,password);
 		pa.click(we.submit);
-		
 		driver.findElement(we.profileicon);
-		
-		
-		
 	}
+	
+	
 }
